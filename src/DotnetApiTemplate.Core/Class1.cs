a6854691469c452060173051dd385e061320a314
Oliver Ward <oliver.ward94@gmail.com>
2026-04-25 16:17:01 +1200

    chore: scaffold ASP.NET core API solution

---
+++ src/DotnetApiTemplate.Core/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace DotnetApiTemplate.Core;
+
+public class Class1
+{
+
+}
---
+++ tests/DotnetApiTemplate.UnitTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace DotnetApiTemplate.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
